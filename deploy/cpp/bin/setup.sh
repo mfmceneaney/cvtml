a0835a671e72e1a68767e395a59152062ac723e6
Matthew McEneaney <matthew.mceneaney@duke.edu>
2023-02-17 18:08:46 -0500

    Changed order of C++ dependency installs in setup scripts.

--- deploy/cpp/bin/setup.sh
+++ deploy/cpp/bin/setup.sh
@@ -5,17 +5,6 @@ export VENV_PACKAGES=/path/to/venv/lib/python3/site-packages
 
 cd $PROJECT_DIR
 
-# Install PyTorch Scatter
-git clone https://github.com/rusty1s/pytorch_scatter.git
-pushd pytorch_sparse
-mkdir build
-cd build
-cmake -DCMAKE_PREFIX_PATH=$PROJECT_DIR/libtorch ..
-make
-mkdir install
-cmake --install . --prefix $PWD/install/
-popd
-
 # Install PyTorch Sparse
 git clone --recurse-submodules https://github.com/rusty1s/pytorch_sparse.git
 pushd pytorch_sparse
@@ -27,8 +16,8 @@ mkdir install
 cmake --install . --prefix $PWD/install/
 popd
 
-# Install PyTorch Spline Conv
-git clone https://github.com/rusty1s/pytorch_spline_conv.git
+# Install PyTorch Scatter
+git clone https://github.com/rusty1s/pytorch_scatter.git
 pushd pytorch_sparse
 mkdir build
 cd build
@@ -40,6 +29,17 @@ popd
 
 # Install PyTorch Cluster
 git clone https://github.com/rusty1s/pytorch_cluster.git
+pushd pytorch_sparse
+mkdir build
+cd build
+cmake -DCMAKE_PREFIX_PATH=$PROJECT_DIR/libtorch ..
+make
+mkdir install
+cmake --install . --prefix $PWD/install/
+popd
+
+# Install PyTorch Spline Conv
+git clone https://github.com/rusty1s/pytorch_spline_conv.git
 pushd pytorch_sparse
 mkdir build
 cd build
